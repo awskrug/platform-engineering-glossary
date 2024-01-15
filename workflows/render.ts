f38f6059dcfe3ebe935a1364735a34028fe2e9f3
jjangga0214 <bnbcmindnpass@gmail.com>
2024-01-15 12:14:23 +0900

    build(render): fix by upath
    dirname(import.meta) returns with \ on Windows,
    while globby, which uses fast-glob under the hood,
    does not treat as path delimiter.
    Thus upath is introduced to solve this issue.
    
    REF: https://github.com/sindresorhus/globby/issues/130
    REF: https://github.com/mrmlnc/fast-glob#how-to-write-patterns-on-windows

--- workflows/render.ts
+++ workflows/render.ts
@@ -1,5 +1,5 @@
-import path from 'node:path'
 import fs from 'node:fs/promises'
+import path from 'upath'
 import { globby } from 'globby'
 import { dirname, filename } from 'dirname-filename-esm'
 import mustache from 'mustache'
