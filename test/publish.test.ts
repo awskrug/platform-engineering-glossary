ead467af78f15f1d7444459d6a91f211be6a1bd4
jjangga0214 <bnbcmindnpass@gmail.com>
2024-01-17 23:17:55 +0900

    ci: publish and comment

--- test/publish.test.ts
+++ test/publish.test.ts
@@ -1,24 +1,215 @@
-import { insertId, parseMarkdown } from '../workflows/publish.js'
+import {
+  insertId,
+  extractFrontMatter,
+  extractRawFrontMatter,
+  setFrontMatter,
+} from '../workflows/publish.js'
+
+describe('extractRawFrontMatter', () => {
+  test('when frontmatter exists', () => {
+    expect(
+      extractRawFrontMatter(`---
+title: platform engineering
+tags: platformengineering,idp
+ci: false
+platform: dev.to
+id: 1234
+---
+
+# Platform Enginering
+`),
+    ).toBe(`title: platform engineering
+tags: platformengineering,idp
+ci: false
+platform: dev.to
+id: 1234
+`)
+
+    expect(
+      extractRawFrontMatter(`
+---
+title: platform engineering
+tags: platformengineering,idp
+ci: false
+platform: dev.to
+id: 1234
+---
+
+# Platform Enginering
+`),
+    ).toBe(`title: platform engineering
+tags: platformengineering,idp
+ci: false
+platform: dev.to
+id: 1234
+`)
+  })
+
+  test('when frontmatter does not exist', () => {
+    expect(extractRawFrontMatter(`# Platform Enginering`)).toBeUndefined()
+    expect(
+      extractRawFrontMatter(`---
+title: platform engineering
+
+# Platform Enginering
+`),
+    ).toBeUndefined()
+
+    expect(
+      extractRawFrontMatter(`title: platform engineering
+---
+
+# Platform Enginering
+`),
+    ).toBeUndefined()
+  })
+
+  test('when frontmatter is empty', () => {
+    expect(extractRawFrontMatter(`---\n---`)).toBeUndefined()
+    expect(extractRawFrontMatter(`---\n\n---`)).toBeUndefined()
+    expect(extractRawFrontMatter(`---\n  \n---`)).toBeUndefined()
+    expect(extractRawFrontMatter(`---\n \n \n---`)).toBeUndefined()
+  })
+})
+
+describe('extractFrontMatter', () => {
+  test('when frontmatter exists', () => {
+    expect(
+      extractFrontMatter(`---
+title: platform engineering
+tags: platformengineering,idp
+ci: false
+platform: dev.to
+id: 1234
+---
+
+# Platform Enginering
+`),
+    ).toStrictEqual({
+      title: 'platform engineering',
+      tags: 'platformengineering,idp',
+      ci: false,
+      platform: 'dev.to',
+      id: 1234,
+    })
+
+    expect(
+      extractFrontMatter(`
+---
+title: platform engineering
+tags: platformengineering,idp
+ci: false
+platform: dev.to
+id: 1234
+---
+
+# Platform Enginering
+`),
+    ).toStrictEqual({
+      title: 'platform engineering',
+      tags: 'platformengineering,idp',
+      ci: false,
+      platform: 'dev.to',
+      id: 1234,
+    })
+  })
+
+  test('when frontmatter does not exist', () => {
+    expect(extractFrontMatter(`# Platform Enginering`)).toBeUndefined()
+    expect(
+      extractFrontMatter(`---
+title: platform engineering
+
+# Platform Enginering
+`),
+    ).toBeUndefined()
+    expect(
+      extractFrontMatter(`title: platform engineering
+---
+
+# Platform Enginering
+`),
+    ).toBeUndefined()
+  })
+
+  test('when frontmatter is empty', () => {
+    expect(extractFrontMatter(`---\n---`)).toBeUndefined()
+    expect(extractFrontMatter(`---\n\n---`)).toBeUndefined()
+    expect(extractFrontMatter(`---\n  \n---`)).toBeUndefined()
+    expect(extractFrontMatter(`---\n \n \n---`)).toBeUndefined()
+    expect(
+      extractFrontMatter(`---
+
+---
+
+# Platform Enginering
+`),
+    ).toBeUndefined()
+  })
+})
+
+describe('setFrontMatter', () => {
+  test('when frontmatter exists', () => {
+    expect(
+      setFrontMatter({
+        frontMatter: {
+          title: 'platform engineering!!!!!!',
+          ci: true,
+          tags: 'idp,platformengineering',
+          slug: 'about-platform-engineering',
+        },
+        content: `---
+title: platform engineering
+tags: platformengineering,idp
+ci: false
+platform: dev.to
+id: 1234
+---
+
+# Platform Enginering
+`,
+      }),
+    ).toBe(`---
+title: platform engineering!!!!!!
+tags: idp,platformengineering
+ci: true
+platform: dev.to
+id: 1234
+slug: about-platform-engineering
+---
+
+# Platform Enginering
+`)
+  })
+})
 
 describe('insertId', () => {
-  // eslint-disable-next-line jest/no-commented-out-tests
-  // test('throwing error when id already exists', () => {
-  //   expect(
-  //     insertId({
-  //       id: 1234,
-  //       content: `---
-  //   title: platform engineering
-  //   id: 4321
-  //   ---
+  test('when id already exists', async () => {
+    await expect(
+      insertId({
+        id: 1234,
+        content: `---
+title: platform engineering
+id: 4321
+---
 
-  //   # Platform Enginering
-  //   `,
-  //     }),
-  //   ).toThrow()
-  // })
+# Platform Enginering
+`,
+      }),
+    ).rejects.toThrow('id already exists.')
+  })
 
-  test('inserting id when id does not exists', () => {
-    expect(
+  test('when frontmatter does not exist', async () => {
+    await expect(
+      insertId({
+        id: 1234,
+        content: '# Platform Enginering',
+      }),
+    ).rejects.toThrow('Frontmatter does not exist.')
+  })
+
+  test('when id does not exist', async () => {
+    await expect(
       insertId({
         id: 1234,
         content: `---
@@ -28,7 +219,7 @@ title: platform engineering
 # Platform Enginering
 `,
       }),
-    ).toBe(`---
+    ).resolves.toBe(`---
 title: platform engineering
 id: 1234
 ---
@@ -36,23 +227,24 @@ id: 1234
 # Platform Enginering
 `)
   })
-})
 
-describe('parseMarkdown', () => {
-  test('parseMarkdown', () => {
-    expect(
-      parseMarkdown(`---
-title: platform engineering
-tags: platformengineering,idp
+  test('with mustache template', async () => {
+    await expect(
+      insertId({
+        id: 1234,
+        content: `---
+title: {{ platform engineering }}
+---
+
+# Platform Enginering
+`,
+      }),
+    ).resolves.toBe(`---
+title: {{ platform engineering }}
 id: 1234
 ---
 
 # Platform Enginering
-`),
-    ).toStrictEqual({
-      title: 'platform engineering',
-      tags: 'platformengineering,idp',
-      id: 1234,
-    })
+`)
   })
 })
--- workflows/render.ts
+++ workflows/render.ts
@@ -4,7 +4,16 @@ import { globby } from 'globby'
 import { dirname, filename } from 'dirname-filename-esm'
 import mustache from 'mustache'
 
-export function parseGlossary(content: string): Record<string, string> {
+export async function parseGlossary(
+  content?: string,
+): Promise<Record<string, string>> {
+  if (!content) {
+    // eslint-disable-next-line no-param-reassign
+    content = await fs.readFile(
+      path.resolve(dirname(import.meta), '../glossary.csv'),
+      'utf8',
+    )
+  }
   return Object.fromEntries(
     content
       .trim()
@@ -14,41 +23,64 @@ export function parseGlossary(content: string): Record<string, string> {
   )
 }
 
-interface RenderOptions {
-  filename: string // full absolute path
-  glossary: Record<string, string>
-}
-
-async function render({ filename, glossary }: RenderOptions): Promise<void> {
-  const content = await fs.readFile(filename, { encoding: 'utf8' })
-  const renderedContent = mustache.render(content, glossary)
-  // '/path/to/foo.md' is rendered as '/path/to/foo.rendered.md'
+/**
+ * @example
+ * toRenderedFilename('/path/to/foo.md') // => '/path/to/foo.rendered.md'
+ */
+export function toRenderedFilename(filename: string): string {
   const parsedFilename = path.parse(filename)
-  const renderedFilename = path.resolve(
+  return path.resolve(
     parsedFilename.dir,
     `${parsedFilename.name}.rendered${parsedFilename.ext}`,
   )
-  await fs.writeFile(renderedFilename, renderedContent, {
-    encoding: 'utf8',
-  })
 }
+
+/**
+ * @example
+ * toOriginalFilename('/path/to/foo.rendered.md') // => '/path/to/foo.md'
+ */
+export function toOriginalFilename(filename: string): string {
+  const { dir, name, ext } = path.parse(filename)
+  return path.resolve(dir, `${name.replace(/\.rendered$/, '')}${ext}`)
+}
+
+// e.g. author is alice when the article is '/path/to/articles/alice/foo.md'
+export function getAuthor(filename: string): string {
+  return path
+    .resolve(filename)
+    .replace(path.resolve(dirname(import.meta), '../articles'), '')
+    .split('/')
+    .find(Boolean)! // This predicate filters out an empty string when created by split('/')
+}
+
+interface RenderOptions {
+  filename: string // full absolute path
+  glossary?: Record<string, string>
+}
+
+export async function render({
+  filename,
+  glossary,
+}: RenderOptions): Promise<string> {
+  // eslint-disable-next-line no-param-reassign
+  glossary = glossary || (await parseGlossary())
+  const content = await fs.readFile(filename, { encoding: 'utf8' })
+  const renderedContent = mustache.render(content, glossary)
+  await fs.writeFile(toRenderedFilename(filename), renderedContent, 'utf8')
+  return renderedContent
+}
+
+export const extensions =
+  '.{md,srt,srt,sbv,sub,lrc,cap,smi,sami,rt,vtt,ttml,dfxp,txt}'
 
 // When the module is imported, do nothing.
 if (process.argv[1] === filename(import.meta)) {
   const dir = dirname(import.meta)
   const articles = await globby([
-    path.resolve(
-      dir,
-      '../articles/*/**/*.{md,srt,srt,sbv,sub,lrc,cap,smi,sami,rt,vtt,ttml,dfxp,txt}',
-    ),
+    path.resolve(dir, `../articles/*/**/*${extensions}`),
     '!**/*.rendered.*',
   ])
 
-  const glossary = parseGlossary(
-    await fs.readFile(path.resolve(dir, '../glossary.csv'), {
-      encoding: 'utf8',
-    }),
-  )
-
+  const glossary = await parseGlossary()
   await Promise.all(articles.map((filename) => render({ filename, glossary })))
 }
